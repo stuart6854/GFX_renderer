6e808797d86ba91538afcf2f233d0efd82fd9a7f
Stuart Millman <stu.millman15@gmail.com>
2021-06-30 13:39:48 +0100

    No longer link debug Assimp + Shaderc dlls (decreased performance)

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,23 +5,25 @@ add_subdirectory(Dependencies/glm)
 add_subdirectory(Dependencies/fmt)
 add_subdirectory(Dependencies/stb)
 
-find_library(assimp_Debug NAMES assimp-vc141-mtd PATHS "Dependencies/assimp/bin/Debug" REQUIRED)
+#find_library(assimp_Debug NAMES assimp-vc141-mtd PATHS "Dependencies/assimp/bin/Debug" REQUIRED)
 find_library(assimp_Release NAMES assimp-vc141-mt PATHS "Dependencies/assimp/bin/Release" REQUIRED)
 add_library(assimp STATIC IMPORTED GLOBAL)
 set_target_properties(assimp PROPERTIES
 	IMPORTED_LOCATION "${assimp_Release}"
-	IMPORTED_LOCATION_DEBUG "${assimp_Debug}"
-	IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+	#IMPORTED_LOCATION "${assimp_Release}"
+	#IMPORTED_LOCATION_DEBUG "${assimp_Debug}"
+	#IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
 )
 target_include_directories(assimp INTERFACE "Dependencies/assimp/include")
 
-find_library(shaderc_Debug NAMES shaderc_sharedd PATHS "Dependencies/VulkanSDK/Lib" REQUIRED)
+#find_library(shaderc_Debug NAMES shaderc_sharedd PATHS "Dependencies/VulkanSDK/Lib" REQUIRED)
 find_library(shaderc_Release NAMES shaderc_shared PATHS "$ENV{VULKAN_SDK}/Lib" REQUIRED)
 add_library(shaderc STATIC IMPORTED GLOBAL)
 set_target_properties(shaderc PROPERTIES
-	IMPORTED_LOCATION_RELEASE "${shaderc_Release}"
-	IMPORTED_LOCATION_DEBUG "${shaderc_Debug}"
-	IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+	IMPORTED_LOCATION "${shaderc_Release}"
+	#IMPORTED_LOCATION_RELEASE "${shaderc_Release}"
+	#IMPORTED_LOCATION_DEBUG "${shaderc_Debug}"
+	#IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
 )
 
 find_library(spirv-cross-core_Debug NAMES spirv-cross-cored PATHS "Dependencies/VulkanSDK/Lib" REQUIRED)
