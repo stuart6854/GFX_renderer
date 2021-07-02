5235896eb088b9fc6f7bae796101c161a2089aa3
Stuart Millman <stu.millman15@gmail.com>
2021-07-02 17:42:30 +0100

    Added CreateBox() to Primitives

--- GFX/include/GFX/Resources/Primitives.h
+++ GFX/include/GFX/Resources/Primitives.h
@@ -12,11 +12,10 @@ namespace gfx
     namespace Primitives
     {
         auto CreatePlane(DeviceContext& deviceCtx, float size = 1.0f, const uint32_t resolution = 1) -> std::shared_ptr<Mesh>;
+        auto CreateBox(DeviceContext& deviceCtx, const glm::vec3& size = { 1.0f, 1.0f, 1.0f }) -> std::shared_ptr<Mesh>;
 
         // static auto CreateCone() -> std::shared_ptr<Mesh>;
         // static auto CreateCylinder() -> std::shared_ptr<Mesh>;
         // static auto CreateTorus() -> std::shared_ptr<Mesh>;
-
-    }  // namespace Primitives
-
-}  // namespace gfx
+    } // namespace Primitives
+}     // namespace gfx
--- GFX/src/GFX/Resources/Primitives.cpp
+++ GFX/src/GFX/Resources/Primitives.cpp
@@ -64,9 +64,100 @@ namespace gfx
         return mesh;
     }
 
+    auto Primitives::CreateBox(DeviceContext& deviceCtx, const glm::vec3& size) -> std::shared_ptr<Mesh>
+    {
+        const auto hS = size / 2.0f;
+
+        const std::vector<Vertex> vertices = {
+            // Front
+            { { -hS.x, hS.y, -hS.z }, { 0, 0, -1 }, { 0, 0 } },
+            { { -hS.x, -hS.y, -hS.z }, { 0, 0, -1 }, { 0, 1 } },
+            { { hS.x, -hS.y, -hS.z }, { 0, 0, -1 }, { 1, 1 } },
+            { { hS.x, hS.y, -hS.z }, { 0, 0, -1 }, { 1, 0 } },
+            // Back
+            { { hS.x, hS.y, hS.z }, { 0, 0, 1 }, { 0, 0 } },
+            { { hS.x, -hS.y, hS.z }, { 0, 0, 1 }, { 0, 1 } },
+            { { -hS.x, -hS.y, hS.z }, { 0, 0, 1 }, { 1, 1 } },
+            { { -hS.x, hS.y, hS.z }, { 0, 0, 1 }, { 1, 0 } },
+            // Left
+            { { -hS.x, hS.y, hS.z }, { -1, 0, 0 }, { 0, 0 } },
+            { { -hS.x, -hS.y, hS.z }, { -1, 0, 0 }, { 0, 1 } },
+            { { -hS.x, -hS.y, -hS.z }, { -1, 0, 0 }, { 1, 1 } },
+            { { -hS.x, hS.y, -hS.z }, { -1, 0, 0 }, { 1, 0 } },
+            // Right
+            { { hS.x, hS.y, -hS.z }, { 1, 0, 0 }, { 0, 0 } },
+            { { hS.x, -hS.y, -hS.z }, { 1, 0, 0 }, { 0, 1 } },
+            { { hS.x, -hS.y, hS.z }, { 1, 0, 0 }, { 1, 1 } },
+            { { hS.x, hS.y, hS.z }, { 1, 0, 0 }, { 1, 0 } },
+            // Top
+            { { -hS.x, hS.y, hS.z }, { 0, 1, 0 }, { 0, 0 } },
+            { { -hS.x, hS.y, -hS.z }, { 0, 1, 0 }, { 0, 1 } },
+            { { hS.x, hS.y, -hS.z }, { 0, 1, 0 }, { 1, 1 } },
+            { { hS.x, hS.y, hS.z }, { 0, 1, 0 }, { 1, 0 } },
+            // Bottom
+            { { -hS.x, -hS.y, -hS.z }, { 0, -1, 0 }, { 0, 0 } },
+            { { -hS.x, -hS.y, hS.z }, { 0, -1, 0 }, { 0, 1 } },
+            { { hS.x, -hS.y, hS.z }, { 0, -1, 0 }, { 1, 1 } },
+            { { hS.x, -hS.y, -hS.z }, { 0, -1, 0 }, { 1, 0 } },
+        };
+
+        const std::vector<uint32_t> indices = {
+            // Front
+            0,
+            1,
+            2,
+            2,
+            3,
+            0,
+            // Back
+            4,
+            5,
+            6,
+            6,
+            7,
+            4,
+            // Left
+            8,
+            9,
+            10,
+            10,
+            11,
+            8,
+            // Right
+            12,
+            13,
+            14,
+            14,
+            15,
+            12,
+            // Top
+            16,
+            17,
+            18,
+            18,
+            19,
+            16,
+            // Bottom
+            20,
+            21,
+            22,
+            22,
+            23,
+            20,
+        };
+
+        auto mesh = std::make_shared<Mesh>(deviceCtx, vertices, indices);
+
+        // TODO: Move uploads to Mesh.cpp
+        deviceCtx.Upload(mesh->GetVertexBuffer().get(), mesh->GetVertices().data());
+        deviceCtx.Upload(mesh->GetIndexBuffer().get(), mesh->GetIndices().data());
+
+        return mesh;
+    }
+
     // auto Primitives::CreateCone() -> std::shared_ptr<Mesh> { return nullptr; }
     //
     // auto Primitives::CreateCylinder() -> std::shared_ptr<Mesh> { return nullptr; }
     //
     // auto Primitives::CreateTorus() -> std::shared_ptr<Mesh> { return nullptr; }
-}  // namespace gfx
+} // namespace gfx
