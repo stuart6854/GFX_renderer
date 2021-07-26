78b5124fb3867e2f29dedb7815764b78f26abb62
Stuart Millman <stu.millman15@gmail.com>
2021-07-26 09:00:10 +0100

    Added format to TextureBuilder

--- GFX/include/GFX/Resources/TextureBuilder.h
+++ GFX/include/GFX/Resources/TextureBuilder.h
@@ -7,13 +7,16 @@
 
 namespace gfx
 {
+    enum class TextureFormat;
+
     class TextureBuilder
     {
     public:
-        TextureBuilder(uint32_t width, uint32_t height);
+        TextureBuilder(uint32_t width, uint32_t height, bool isHdr = false);
 
         auto GetWidth() const -> uint32_t { return m_width; }
         auto GetHeight() const -> uint32_t { return m_height; }
+        auto GetFormat() const -> TextureFormat { return m_format; }
 
         auto GetData() const -> const std::vector<uint8_t>& { return m_data; }
 
@@ -23,6 +26,7 @@ namespace gfx
     private:
         uint32_t m_width = 0;
         uint32_t m_height = 0;
+        TextureFormat m_format;
 
         std::vector<uint8_t> m_data = {};
     };
