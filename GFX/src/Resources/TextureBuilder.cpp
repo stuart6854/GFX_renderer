bca898a16b9f08438f173f416dd3c7db69f4abb7
Stuart Millman <stu.millman15@gmail.com>
2021-08-13 07:23:28 +0100

    Updated TextureBuilder

--- GFX/src/Resources/TextureBuilder.cpp
+++ GFX/src/Resources/TextureBuilder.cpp
@@ -3,18 +3,20 @@
 #include "GFX/Debug.h"
 #include "GFX/Resources/Texture.h"
 
+#include <algorithm>
+
 namespace gfx
 {
-    inline auto ConvertColorValue(const float value) -> uint8_t
+    inline auto FloatToByte(const float value) -> uint8_t
     {
-        return static_cast<uint8_t>(std::floor(value >= 1.0f ? 255 : value * 256.0f));
+        return uint8_t(std::clamp(256.0f * value, 0.0f, 255.0f));
     }
 
     TextureBuilder::TextureBuilder(const uint32_t width, const uint32_t height, bool isHdr)
         : m_width(width),
           m_height(height)
     {
-        if(!isHdr) m_format = TextureFormat::eRGBA;
+        if (!isHdr) m_format = TextureFormat::eRGBA;
 
         m_data.resize(width * height * 4);
     }
@@ -29,10 +31,10 @@ namespace gfx
         {
             const auto& color = colors[i];
 
-            m_data[i * 4] = ConvertColorValue(color.r);
-            m_data[i * 4 + 1] = ConvertColorValue(color.g);
-            m_data[i * 4 + 2] = ConvertColorValue(color.b);
-            m_data[i * 4 + 3] = ConvertColorValue(color.a);
+            m_data[i * 4] = FloatToByte(color.r);
+            m_data[i * 4 + 1] = FloatToByte(color.g);
+            m_data[i * 4 + 2] = FloatToByte(color.b);
+            m_data[i * 4 + 3] = FloatToByte(color.a);
         }
     }
 
@@ -40,9 +42,9 @@ namespace gfx
     {
         const auto baseIndex = (x + y * m_width) * 4;
 
-        m_data[baseIndex] = ConvertColorValue(color.r);
-        m_data[baseIndex + 1] = ConvertColorValue(color.g);
-        m_data[baseIndex + 2] = ConvertColorValue(color.b);
-        m_data[baseIndex + 3] = ConvertColorValue(color.a);
+        m_data[baseIndex] = FloatToByte(color.r);
+        m_data[baseIndex + 1] = FloatToByte(color.g);
+        m_data[baseIndex + 2] = FloatToByte(color.b);
+        m_data[baseIndex + 3] = FloatToByte(color.a);
     }
 }
